4737ca1bddb6c9591ca7812811be4f1df0d3151e
Rory Tanner <roryscoding@gmail.com>
2021-01-06 12:01:38 +1100

    added webpack.config.js file and added entry and plugin code

---
+++ public/webpack.config.js
@@ -0,0 +1,48 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+  entry: {
+    app: './public/index.js',
+    },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: 'BudgetTracker',
+      short_name: 'Budget',
+      description: 'An application that allows you to manage a light budget of simple money in and money out with and without an active connection',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      'theme-color': '#ffffff',
+      start_url: '/',
+      icons: [
+        {
+          src: path.resolve('public/icons/icon-192x192.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('public', 'icons'),
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = config;
